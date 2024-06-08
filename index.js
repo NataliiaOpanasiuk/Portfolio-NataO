2ee885baf9f05582331bb56edad2deb4221e2f81
NataliiaOpanasiuk <nataopanasiuk@gmail.com>
2024-06-07 17:51:59 -0700

    Added image gallery scroll

---
+++ index.js
@@ -0,0 +1,21 @@
+function handleLeftClick() {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft -= 320;
+}
+function handleRightClick() {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft += 320;
+}
+function handleScrollEvent(event) {
+  event.preventDefault();
+  scrollContainer.scrollLeft += event.deltaY;
+  scrollContainer.style.scrollBehavior = "auto";
+}
+
+let shevronLeft = document.querySelector(".fa-circle-chevron-left");
+shevronLeft.addEventListener("click", handleLeftClick);
+
+let shevronRight = document.querySelector(".fa-circle-chevron-right");
+shevronRight.addEventListener("click", handleRightClick);
+
+let scrollContainer = document.querySelector(".scroll-gallery");
